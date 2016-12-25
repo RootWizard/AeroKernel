24c3051a33845613c5deab3ebca551ab02c355cb
RootWizard <coreymill01@gmail.com>
2016-12-24 22:14:59 -0800

    Fixes to script

--- aero.sh
+++ aero.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export ARCH=arm
-export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36'
 export CROSS_COMPILE="ccache $HOME/android/kernel/arm-eabi-6.x/bin/arm-eabi-"
 echo "Cleaning up..."
 make clean && make mrproper
@@ -11,7 +11,7 @@ make -j12
 if [ ! -f ./arch/arm/boot/zImage-dtb ]; then 
 	echo "Build has failed, no zImage found..."
 else
-	echo "Done compiling, compying to anykernel and zipping..."
+	echo "Done compiling, copying to anykernel and zipping..."
 	mv ./arch/arm/boot/zImage-dtb $HOME/android/kernel/anykernel
 	cd $HOME/android/kernel/anykernel 
 	mv .git $HOME/android/kernel/temp && mv README.md $HOME/android/kernel/temp
